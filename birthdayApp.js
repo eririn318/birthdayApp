890b127d21ebecdd8e9aef24407da56896f6fef4
LA Tech <eririn75@hotmail.com>
2025-02-17 15:20:25 -0800

    Add files via upload

---
+++ birthdayApp.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js"
+import { getDatabase,
+         ref,
+         push } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURL: "https://birthday-637a8-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const referenceInDB = ref(database, "birthdays")
+
+const birthdayInputField = document.getElementById("birthday-input")
+const submitButton = document.getElementById("submit-button")
+
+submitButton.addEventListener("click", function() {
+    push(referenceInDB, birthdayInputField.value)
+    birthdayInputField.value = ""
+})
